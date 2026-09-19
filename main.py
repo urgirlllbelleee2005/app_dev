b5331d77e048bcddaf50b0b602422cd1845891b3
urgirlllbelleee <castriel21@gmail.com>
2026-09-20 03:57:25 +0800

    feat: add initial sample code

---
+++ main.py
@@ -0,0 +1,13 @@
+# Simple Inventory Program
+
+product_name = "Notebook"
+quantity = 10
+price = 50
+
+total = quantity * price
+
+print("=== INVENTORY SYSTEM ===")
+print("Product:", product_name)
+print("Quantity:", quantity)
+print("Price: ₱", price)
+print("Total Value: ₱", total)
